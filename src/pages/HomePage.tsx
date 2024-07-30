3ed186d6f1b184002525fffa7e7d037a4a844bd6
Artem <tema.palamar4uck@yandex.ru>
2024-07-30 18:26:06 +0300

    Добавился tailwind

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export function HomePage() {
+    return (
+        <div className="m-20">
+            <h1 className="text-7xl text-center font-medium">
+                hello how are you
+            </h1>
+        </div>
+    );
+}
